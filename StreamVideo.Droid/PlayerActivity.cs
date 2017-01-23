68aad5de3eb1d78dcf8d3881f2ff02f58164f019
igr777 <igor@ego-cms.com>
2017-01-23 17:40:13 +0300

    Added Android project

---
+++ StreamVideo.Droid/PlayerActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Net;
+using Android.OS;
+using Android.Widget;
+
+namespace StreamVideo.Droid
+{
+    [Activity(Label = "PlayerActivity")]
+    public class PlayerActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.Player);
+
+            VideoView vidView = (VideoView)FindViewById(Resource.Id.myVideo);
+            //var vidAddress = "http://gourl.su/ud?UKROFASCHIZM.m3u8";
+            var vidAddress = Intent.GetStringExtra("url"); 
+            var vidUri = Uri.Parse(vidAddress);
+
+            vidView.SetVideoURI(vidUri);
+            vidView.Start();
+        }
+    }
+}
---
+++ StreamVideo.Droid/URLActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Net;
+using Android.Content;
+
+namespace StreamVideo.Droid
+{
+    [Activity(Label = "StreamVideo.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class URLActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.URL);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            var streamURL = FindViewById<EditText>(Resource.Id.streamURL);
+            var button = FindViewById<Button>(Resource.Id.playButton);
+
+            streamURL.Text = "http://gourl.su/ud?UKROFASCHIZM.m3u8";
+
+            button.Click += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(streamURL.Text))
+                {
+                    var intent = new Intent(this, typeof(PlayerActivity));
+                    intent.PutExtra("url", streamURL.Text);
+                    StartActivity(intent);
+                }
+                else {
+                    Toast.MakeText(this, "Enter stream url first!", ToastLength.Long);
+                }
+            };
+        }
+    }
+}
+
